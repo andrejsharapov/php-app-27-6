9e8a3fc84a7ce93cb59b89dac3dce7df6098193d
Andrej Sharapov <an.sharapov90@gmail.com>
2022-12-13 00:04:21 +0300

    dev: add user role

--- database/db.sql
+++ database/db.sql
@@ -1,10 +1,10 @@
 CREATE TABLE users
 (
-    id       SERIAL PRIMARY KEY,
+    id       serial PRIMARY KEY,
     name     VARCHAR(256) NOT NULL,
     email    VARCHAR(256),
     password VARCHAR(30)  NOT NULL,
-    date     DATE         NOT NULL
+    date     DATE         not null
 );
 
 --
@@ -18,7 +18,24 @@ DELETE FROM users;
 ALTER TABLE users AUTO_INCREMENT = 1;
 
 --
-SELECT * FROM users;
+DROP TABLE users;
 
 --
-DROP TABLE users;
+ALTER TABLE users
+    ADD COLUMN role VARCHAR(30)
+--     DROP COLUMN role
+;
+
+--
+ALTER TABLE users
+    ALTER role
+        SET DEFAULT 'user'
+;
+
+--
+UPDATE users
+SET role = 'user'
+WHERE id = 1;
+
+--
+SELECT * FROM users;
