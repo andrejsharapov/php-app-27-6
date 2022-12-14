e351e44626fa7d9cb5027fbf9f322fdf81aca5e2
Andrej Sharapov <44722574+andrejsharapov@users.noreply.github.com>
2022-12-14 17:47:01 +0300

    dev(wip): add Monolog\Logger

--- forms/auth.php
+++ forms/auth.php
@@ -1,20 +1,26 @@
 <?php
 session_start();
 
+//use Monolog\Logger;
+//use Monolog\Handler\StreamHandler;
+
 require_once __DIR__ . '/../database/db.php';
 
 $name = $_POST['login'];
-$password = openssl_digest($_POST['password'], "sha512");
+$password = !empty($_POST['password']) ? openssl_digest($_POST['password'], "sha512") : null;
 $token = $_POST['token'];
 
+// check token
 if ($_POST["token"] == $_SESSION["CSRF"]) {
     // get user info
     $findUserName = "SELECT * FROM " . $db_table . " WHERE `name` = '$name' AND `password` = '$password'";
     $auth = mysqli_query($db_link, $findUserName) or die(mysqli_error($db_link));
 
+    // check user in database
     if (mysqli_num_rows($auth) > 0) {
         $user = mysqli_fetch_assoc($auth);
 
+        // write data to session
         $_SESSION['user'] = [
             'id' => $user['id'],
             'name' => $user['name'],
@@ -25,10 +31,22 @@ if ($_POST["token"] == $_SESSION["CSRF"]) {
         ];
 
         $_SESSION['checkAuth'] = 'Авторизация прошла успешно.';
-        header('location: /hello.php');
 
+        header('location: /hello.php');
     } else {
+        // create new logger
+//        $log = new Logger('AUTH LOGGER');
+
+        // set handlers
+//        $log->pushHandler(new StreamHandler(__DIR__ . 'auth.log', Logger::INFO));
+//        $log->pushHandler(new StreamHandler(__DIR__ . 'warn.log', Logger::WARNING));
+
+        // add records
+//        $log->info('Ошибка авторизации', array('user' => $name, 'datetime' => (new DateTime())->format('Y-m-d H:i:s'), 'password' => $password));
+        //$log->warning('Предупреждение');
+
         $_SESSION['checkAuth'] = 'Не верный логин или пароль.';
+
         header('location: /');
     }
 }
